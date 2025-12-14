1bb187fa1748c2dccd1781aa68e72d9e0b606e7c
Dimcirui <dimcirui@qq.com>
2025-12-14 17:11:14 -0500

    Bump version to 1.4.2 in __init__.py and blender_manifest.toml

--- addons/Modder_Batch_Tool/__init__.py
+++ addons/Modder_Batch_Tool/__init__.py
@@ -20,7 +20,7 @@ bl_info = {
     "name": "Modder Batch Tool",
     "author": "诸葛不太亮, Dimcirui",
     "blender": (4, 2, 0),
-    "version": (1, 4, 1),
+    "version": (1, 4, 2),
     "description": "Utility tools to do a lot of repetitive operations automatically.",
     "warning": "",
     "wiki_url": "https://github.com/Dimcirui/Modder_Batch_Tool",
